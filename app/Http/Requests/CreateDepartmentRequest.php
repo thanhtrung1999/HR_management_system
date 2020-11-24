243e068bc25b91392f31122151285df12bb487f2
thanhtrung1999 <darkprince411999@gmail.com>
2020-11-24 17:28:01 +0700

    Add new request: CreateDepartmentRequest.php

---
+++ app/Http/Requests/CreateDepartmentRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateDepartmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'department_name' => 'required|unique:departments,name',
+            'manager' => 'required|gt:0'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'department_name.required' => 'Trường này là bắt buộc',
+            'department_name.unique' => 'Phòng ban đã tồn tại',
+            'manager.required' => 'Trường này là bắt buộc',
+            'manager.gt' => 'Trường này là bắt buộc'
+        ];
+    }
+}
