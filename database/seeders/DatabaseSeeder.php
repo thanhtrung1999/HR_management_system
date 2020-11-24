8658c6f7bb2dead15fcc1d9a2ef85df17d945d07
thanhtrung1999 <darkprince411999@gmail.com>
2020-11-24 17:21:47 +0700

    modify DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            Root::class,
+            RootSeeder::class,
+            EmployeeSeeder::class
         ]);
     }
 }
