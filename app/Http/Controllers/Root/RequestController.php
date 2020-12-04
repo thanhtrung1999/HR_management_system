38e9cbf7a073923bc439b84171729d5ed6c366e1
thanhtrung1999 <darkprince411999@gmail.com>
2020-12-04 17:13:42 +0700

    Completed the function: Managing the list requests of employees (Actor root)

--- app/Http/Controllers/Root/RequestController.php
+++ app/Http/Controllers/Root/RequestController.php
@@ -7,79 +7,34 @@ use Illuminate\Http\Request;
 
 class RequestController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
-    public function index()
-    {
-        return view('root.requests.index');
+    public function getListRequests(){
+        $requests = $this->requestModel->getListRequests();
+        return view('root.requests.index', [
+            'requests' => $requests
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function approvalRequest($id){
+        $request = $this->requestModel->getRequestsById($id);
+        if ($request->approval_by == 1){
+            return redirect()->back()->with('error', 'Yêu cầu đã được trưởng phòng duyệt');
+        }
+        $request->status = 1;
+        $request->approval_by = 2;
+        $request->save();
+
+        return redirect()->back()->with('success', 'Duyệt yêu cầu thành công');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    public function cancelRequest($id){
+        $request = $this->requestModel->getRequestsById($id);
+        if ($request->approval_by == 1){
+            return redirect()->back()->with('error', 'Yêu cầu đã được trưởng phòng duyệt');
+        }
+        $request->status = 2;
+        $request->approval_by = 2;
+        $request->save();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return redirect()->back()->with('success', 'Hủy yêu cầu thành công');
     }
 }
